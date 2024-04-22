b2b0b60c28e28aacbe1ae0f918c08ab93336d030
Wora-Ben <ahmedoussamabendaou@gmail.com>
2024-04-22 18:33:24 +0200

    Fix

---
+++ model/ClientModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace model;
+
+class ClientModel
+{
+    public $id_client;
+    public $raison_sociale_client;
+    public $adresse_client;
+    public $telephone_client;
+    public $email_client;
+    public $n_siren;
+    public $mode_paiement;
+    public $delai_paiement;
+    public $mode_livraison;
+}
